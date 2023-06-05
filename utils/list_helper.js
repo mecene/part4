aa20fd69750268151ec4836cea141218cbcfd9c3
mecene <mecene.co@gmail.com>
2023-06-05 12:56:03 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    const likes = blogs.map(value => value.likes)
+    return likes.reduce((a,c) => a + c, 0)
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
