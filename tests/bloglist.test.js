f2ac346aca5adac5c536eb0626e643ee7ac67602
mecene <mecene.co@gmail.com>
2023-06-07 12:24:03 +0200

    4.5

--- tests/bloglist.test.js
+++ tests/bloglist.test.js
@@ -90,3 +90,36 @@ describe('total likes', () => {
     })
 
   })
+
+  describe('Favorite article', () => {
+
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      },
+    ]
+
+    test('Of a the most likes article (favorite)', () => {
+      const result = listHelper.favoriteBlog(blogs)
+      console.log(`The total of likes is ${result}`);
+      expect(result).toEqual(
+        {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12,
+        })
+    })
+  })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,8 +6,19 @@ const dummy = (blogs) => {
     const likes = blogs.map(value => value.likes)
     return likes.reduce((a,c) => a + c, 0)
   }
+
+  const favoriteBlog  = (blogs1) => {
+    const mostLikes = blogs1.reduce((a, c) => {
+      console.log(a.likes, c.likes)
+      return a.likes >  c.likes ? a : c
+     
+    })
+    const {_id, url,__v, ...mostLikesReduced} = mostLikes
+    return mostLikesReduced
+  }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
